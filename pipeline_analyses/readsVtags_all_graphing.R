33d8a64af44c89e581c6940347c5c969668cff97
mfisher5@uw.edu <mfisher5@uw.edu>
2017-08-16 12:49:51 -0700

    finish r graphs for reads / tags counts

--- pipeline_analyses/readsVtags_all_graphing.R
+++ pipeline_analyses/readsVtags_all_graphing.R
@@ -57,12 +57,59 @@ legend("topleft", legend = c("reg", "combo", "contam", "pcontam", "part deg", "a
 x <- readsVtags_all_meta$Pop_Code
 y <- readsVtags_all_meta$n_tags/1000
 
-plot(x,y, main = "Number of Tags per Sample (ustacks)", xlab = "Population", ylab= "# tags (x 1000)", col=alpha(colors, 0.75), pch = points, xaxt = "n", yaxt = "n", ylim = c(0, 18), cex = 1.5)
+plot(x,y, main = "Number of Tags per Sample (ustacks)", xlab = "Population", ylab= "# tags (x 1000)", col=alpha(colors, 0.75), pch = points, xaxt = "n", , yaxt = "n", cex = 1.5)
 axis(1, at = seq(1,10, by=1), labels = c("YS", "BOR", "GE15", "GE14", "NAM", "JBE", "JBL", "POH", "JUK", "UP"))
-axis(2, at = seq(0, 18, by = 1))
+axis(2, at = seq(0, 150, by = 25))
 abline(v = c(1,2,3,4,5,6,7,8,9,10), col = "gray", lty = 3)
-legend("topleft", legend = c("reg", "combo", "contam", "pcontam", "part deg", "alaska"), col = c("darkblue", "darkcyan", "firebrick", "coral2", "darkgoldenrod", "gray45"), pch = 19)
-?legend
+legend(x = 5, y = 150, legend = c("reg", "combo", "contam", "pcontam", "part deg", "alaska"), col = c("darkblue", "darkcyan", "firebrick", "coral2", "darkgoldenrod", "gray45"), pch = 19)
+
+
+
+
+
+################# plotting number of reads v. number of loci ###################
+x <- readsVtags_all_meta$n_reads/1000000
+y <- readsVtags_all_meta$n_tags/1000
+
+plot(x,y, main = "Number of Reads v. Number of Tags", xlab = "# Reads (x 1mill)", ylab= "# tags (x 1000)", col=alpha(colors, 0.75), pch = points, cex = 1.5, xlim = c(0,18), xaxt = "n", yaxt = "n")
+axis(1, at = seq(0,18, by=1))
+axis(2, at = seq(0, 150, by = 25))
+legend("topright", legend = c("reg", "combo", "contam", "pcontam", "part deg", "alaska"), col = c("darkblue", "darkcyan", "firebrick", "coral2", "darkgoldenrod", "gray45"), pch = 19)
+
+
+
+
+
+############# comparing the new lane 5 samples to all other samples ###############
+
+
+readsVtags_all_meta <- read_delim("D:/Pacific cod/DataAnalysis/PCod-Korea-repo/readsVtags_all_meta.txt", "\t", escape_double = FALSE, trim_ws = TRUE)
+View(readsVtags_all_meta)
+
+
+
+lanes <- readsVtags_all_meta$lane
+colors2 = c()
+for(i in lanes){
+  if(i == 5){
+    colors2 <- c(colors2,"darkcyan")
+  } 
+  else {
+    colors2 <- c(colors2,"gray45")
+  }
+}
+
+x <- readsVtags_all_meta$n_reads/1000000
+y <- readsVtags_all_meta$n_tags/1000
+
+plot(x,y, main = "Number of Reads v. Number of Tags", xlab = "# Reads (x 1mill)", ylab= "# tags (x 1000)", col=alpha(colors2, 0.75), pch = 19, cex = 1.5, xlim = c(0,18), xaxt = "n", yaxt = "n")
+axis(1, at = seq(0,18, by=1))
+axis(2, at = seq(0, 150, by = 25))
+legend("topright", legend = c("new sample", "existing sample"), col = c("darkcyan","gray45"), pch = 19)
+
+
+
+
 
 
 
