1138ab2fa7c3f254e0d8a5c2ef7bf4ae5e9c7f1a
mfisher5@uw.edu <mfisher5@uw.edu>
2018-04-11 16:53:13 -0700

    genotype error plotting

--- analyses/R/Genotyping_Error_MF.R
+++ analyses/R/Genotyping_Error_MF.R
@@ -6,6 +6,8 @@
 
 library(readr)
 library(dplyr)
+setwd("D:/Pacific cod/DataAnalysis/PCod-Korea-repo")
+
 
 
 # Read in data ------------------------------------------------------------
@@ -47,6 +49,58 @@ mydata_filtered %>% group_by(type) %>% summarise(avg = mean(p.mismatch))
 
 
 
+# Plot DAPC to Compare All Replicates ------------------------------------
+## to do this, you need a genepop file containing your replicates 
+library(ape)
+library(ade4)
+library(adegenet)
+mydata <-read.genepop("stacks_b8_verif/batch_8_replicates_south_DAPC.gen")
+## run dapc
+mydata_dapc <- dapc(mydata,mydata$pop,n.pca=234,n.da=5) 
+## find optimal number of principal components
+test_a_score <- optim.a.score(mydata_dapc) 
+## run dapc only on optimal number of principal components
+mydata_dapc <- dapc(mydata,mydata$pop,n.pca=14,n.da=5)
+
+## plot
+#pop_cols <- c("gray45", "deepskyblue","mediumorchid2", "mediumorchid4", "chartreuse","coral1","firebrick4")
+#col_leg <- c("firebrick4","coral1","mediumorchid4", "mediumorchid1","deepskyblue", "gray","chartreuse")
+
+pop_cols <- c("gray45", "deepskyblue","deepskyblue4","mediumorchid2", "mediumorchid4")
+col_leg <- c("mediumorchid4", "mediumorchid1","deepskyblue","deepskyblue4", "grey45")
 
 
+scatter(mydata_dapc,scree.da=FALSE, posi.da="bottomright", scree.pca = FALSE, posi.pca = "bottomleft", cellipse=0,leg=FALSE,label=c("", "", "", "", ""), pch = 19,csub=2,cex=1.5,col = pop_cols, clabel=1,solid=0.7, inset.solid=0.7)
+
+legend (x=-13, y=11.5, legend = c( "Geoje '14-'15", "Geoje '13-'14", "Jin. Bay Dec.", "Jin. Bay Feb.","Pohang"), col = col_leg, border = FALSE, bty = "n", cex = 0.9, pt.cex=1.5, y.intersp = 1, title = "Sampling Site",pch=19)
+
+
+
+# DAPC to Compare 300ng & 500ng --------------------------------------------------------------------
+mydata <-read.genepop("stacks_b8_verif/batch_8_filteredMAF_filteredIndivids30_filteredLoci_filteredHWE_filteredCR_south_nomigrants_300ng.gen")
+summary(mydata)
+## run pca
+X <- scaleGen(mydata, NA.method="mean")
+pca_all <- dudi.pca(X,cent=FALSE,scale=FALSE)
+barplot(pca_all$eig[1:50],main="PCA eigenvalues", col=heat.colors(50))
+summary(pca_all)
+pca_all <- dudi.pca(X,cent=FALSE,scale=FALSE, nf=2, scannf=FALSE)
+
+
+
+
+## create the population labels for the legend and set the colors for each population##
+col <- c("darkgray","mediumorchid1","aquamarine2","deepskyblue", "deepskyblue4","mediumorchid4", "orange")
+pop_labels <- c("Pohang '15", "Geoje '15", "Namhae '15", "Jin. Bay '07 Dec.", "Jin. Bay '07 Feb.", "Geoje '14", "Geoje '14 500ng")
+col_leg <- c("mediumorchid4", "mediumorchid1","orange","deepskyblue","deepskyblue4", "grey45")
+s.class(pca_all$li, fac=pop(mydata), 
+        col=col, #color of points. will retain lines between points
+        clabel=0, #remove population labels
+        cellipse=0, #remove ellipses; to add back in, make >=1
+        cpoint=1.5,
+        grid=FALSE, #otherwise will have light gray grid markers
+        pch=19, #change point shapes
+        axesell=TRUE)
+
+legend (x=35, y=45, legend = c( "Geoje '14-'15", "Geoje '13-'14", "Geoje '13-'14 500ng","Jin. Bay Dec.", "Jin. Bay Feb.","Pohang"), col = col_leg, border = FALSE, bty = "n", cex = 0.9, pt.cex=1.5, y.intersp = 1, title = "Sampling Site",pch=19)
 
