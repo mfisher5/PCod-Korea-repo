708dff4dd48da753e2f4bbae4015fa44380d415a
mfisher5@uw.edu <mfisher5@uw.edu>
2018-04-12 14:28:40 -0700

    finish annotation; remove oto data

--- analyses/R/Fis_diveRsity_PCod_MF.R
+++ analyses/R/Fis_diveRsity_PCod_MF.R
@@ -8,33 +8,46 @@
 
 # Import these libraries
 install.packages("diveRsity")
+install.packages("adegenet")
 library("diveRsity")
-install.packages("xlsx")
-install.packages("sendplot")
-install.packages("plotrix")
-install.packages("HWxtest")
-library("xlsx")
-library("sendplot")
-library("plotrix")
-library("HWxtest")
+library("adegenet")
+library(dplyr)
 
 
 # Set working directory
 setwd("D:/Pacific cod/DataAnalysis/PCod-Korea-repo/analyses")
 
+
+# diveRsity ---------------------------------------------------------------
 # Calculate basic descriptive population parameters from a genepop genotype file: Fis per population
-basicStats(infile = "../stacks_b8_wgenome/batch_8_filteredMAF_filteredIndivids30_filteredLoci_filteredHWE_filteredRepsC.gen", outfile = "Hierfstat_diveRsity/stacks_b8_final_filtered_diveRsity_basicStats.txt", fis_ci = TRUE, fis_boots = 1000, fis_alpha = 0.05)
+mydata_stats <- basicStats(infile = "../stacks_b8_verif/batch_8_filteredMAF_filteredIndivids30_filteredLoci_filteredHWE_filteredCR.gen")
+str(mydata_stats)
 
 
-# A minimal function for the calculate of Weir & Cockerham's (2984) Fst and Fit from codominant molecular data: Fit per locus
+
+
+# Heterozygosity ---------------------------------------------------------
+mydata_het <- mydata_stats$obs_het
+
+pops <- colnames(mydata_het)[2:length(colnames(mydata_het))]
+
+colMeans(mydata_het[,2:length(colnames(mydata_het))])
+
+
+### If you run option 5 >> suboption 2 in genepop (.DIV output file), the `1-Qintra` estimate across all loci should equal colMeans output
 
 
 
-# ---------------------------------------------------------------------------------
 
-# Set working directory
-setwd("D:/Pacific cod/DataAnalysis/PCod-Compare-repo/analyses")
 
-# Calculate basic descriptive population parameters from a genepop genotype file: Fis per population
-basicStats(infile = "../stacks_b4_wgenome/batch_4_filteredMAF_filteredLoci50_filteredIndivids50_filteredHWE_genepop.txt", outfile = "b4_final_filtered_diveRsity_basicStats.txt", fis_ci = TRUE, fis_boots = 1000, fis_alpha = 0.05)
+
+# Adegenet ----------------------------------------------------------------
+
+mydata <-read.genepop("../stacks_b8_verif/batch_8_filteredMAF_filteredIndivids30_filteredLoci_filteredHWE_filteredCR.gen")
+mydata_summary <- summary(mydata)
+str(mydata_summary)
+
+
+
+
 
